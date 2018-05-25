7010dd02d14b436b0542222755c0a96976f3073f
Paul Steimel <paulsteimel@gmail.com>
2018-05-25 15:24:33 -0500

    Testing comments

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
 public class Main {
 
+    /*
+    This is the first line of a comment
+    This is the second line of a comment
+     */
+
     public static void main(String[] args) {
-	    System.out.println("Hello World One");
-        System.out.println("Hello World Two");
-        System.out.println("Hello World Three");
+
+        // This is a stand alone comment
+        System.out.println("Hello World One"); // This is a comment
+        //System.out.println("Hello World Two");
+        System.out.println(/*"Hello World Three"*/"This is different text");
     }
 }
